d36bee69b34af20b68329446d356f2a9a739afde
Asichurter <1090489718@qq.com>
2019-09-17 22:37:57 +0800

    transformer test 0917 commit

--- modules/runnable/dlExp15.py
+++ modules/runnable/dlExp15.py
@@ -1,10 +1,12 @@
 import torch as t
 import torch.nn as nn
 from torch.optim import SGD
+from torch.nn.init import normal_
 import numpy as np
 import random as rd
 import math
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier as KNN
 
 def get_datas(c, R, num):
     datas = []
@@ -15,31 +17,49 @@ def get_datas(c, R, num):
         datas.append(data)
     return datas
 
-def plot(p, num, title, group=3, colors=['red','blue','green']):
-    plt.title(title)
-    for g in range(group):
-        plt.scatter([x[0] for x in p[g*num:(g+1)*num]],
-                 [x[1] for x in p[g*num:(g+1)*num]],
-                 marker='o',
-                 color=colors[g])
+def plot(ps, num, title, group=3, colors=['red','blue','green'], marker=['o','x']):
+    if title is not None:
+        plt.title(title)
+    for i,p in enumerate(ps):
+        for g in range(group):
+            plt.scatter([x[0] for x in p[g*num[i]:(g+1)*num[i]]],
+                     [x[1] for x in p[g*num[i]:(g+1)*num[i]]],
+                     marker=marker[i],
+                     color=colors[g])
     plt.show()
 
-def get_loss(p, num, alpha=0.1, beta=0.1, margin=1):
+def get_loss(p, num, alpha=0.01, beta=0.01, margin=1):
     p = p.view(-1,num)
     p_inner_var = p.var(dim=1).sum()
     p_outer_var = p.mean(dim=1).var()
     return alpha*p_inner_var - beta*p_outer_var + margin
 
+def get_block(in_feature, out_feature, relu=True):
+    block_parts = [nn.Linear(in_feature, out_feature)]
+    if relu:
+        block_parts.append(nn.ReLU())
+    return nn.Sequential(*block_parts)
 
-centers = [[0,2],[-2,0],[2,0]]
-r = 3
+
+centers = [[0,5],[0,3],[0,-1]]
+groups = 3
+r = 4
 point_num = 50
 episode = 20
-lr = 0.1
+lr = 1
+test_num = 20
 
 feature_size = [2, 16, 16, 2]
-transformers = [nn.Linear(feature_size[i], feature_size[i+1]) for i in range(len(feature_size)-1)]
+transformers = [get_block(feature_size[i], feature_size[i+1], relu=False) #(i==len(feature_size)-2)
+                                            for i in range(len(feature_size)-1)]
 transformer = nn.Sequential(*transformers)
+
+for name,par in transformer.named_parameters():
+    if name.find("weight") != -1:
+        print(name)
+        normal_(par, 0, 0.1)
+    if name.find('bias') != -1:
+        par.data.fill_(0)
 
 opt = SGD(transformer.parameters(), lr=lr)
 
@@ -47,12 +67,27 @@ for i in range(episode):
     points = []
     for c in centers:
         points += get_datas(c, r, point_num)
-    # plot(points, point_num, "%dth before transformation"%(i+1), group=3)
-    points = t.Tensor(points)
-    points.requires_grad_(True)
+    test_x = []
+    for c in centers:
+        test_x += get_datas(c, r, test_num)
+    train_labels = np.concatenate([np.array([i]*point_num) for i in range(groups)], axis=0)
+    test_labels = np.concatenate([np.array([i]*test_num) for i in range(groups)], axis=0)
+
+    knn = KNN(n_neighbors=5)
+    knn.fit(points, train_labels)
+    predict = knn.predict(test_x)
+
+    acc = (predict==test_labels).sum()/len(predict)
+    plot([points, test_x],
+         [point_num, test_num],
+         title="before %dth transformation\nacc=%.4f"%(i+1, acc),
+         marker=['o','x'])
+
+    points_t = t.Tensor(points)
+    points_t.requires_grad_(True)
 
     transformer.zero_grad()
-    transformed_points = transformer(points)
+    transformed_points = transformer(points_t)
     loss = get_loss(transformed_points, point_num)
 
     print(i, loss)
@@ -60,7 +95,32 @@ for i in range(episode):
     loss.backward()
     opt.step()
 
-    plot(transformed_points.detach().numpy(), point_num, '%dth transformation'%(i+1))
+    # plot(transformed_points.detach().numpy(), point_num, 'after %dth transformation' % (i + 1), show=True)
+
+    knn = KNN(n_neighbors=5)
+    test_x = t.Tensor(test_x)
+    test_x.requires_grad_(False)
+    test_x = transformer(test_x).detach().numpy()
+    transformed_points = transformed_points.detach().numpy()
+
+    knn.fit(transformed_points, train_labels)
+    predict = knn.predict(test_x)
+
+    acc = (predict==test_labels).sum()/len(predict)
+    # print('acc:', acc)
+
+    # test_points = np.concatenate([transformed_points.detach().numpy()[i*point_num:i*point_num+test_num]
+    #                               for i in range(groups)], axis=0)
+    plot([transformed_points, test_x],
+         [point_num, test_num],
+         title="after %dth transformation\nacc=%.4f\nloss=%.4f"%(i+1, acc, loss),
+         marker=['o','x'])
+
+
+
+
+    
+
 
 
 
